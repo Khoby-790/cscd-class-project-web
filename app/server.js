e48672b47a51583394e88e260d49f4a5fd2e0d27
Domey Ben <domeybenjamin1@gmail.com>
2019-10-10 13:44:03 +1200

    WORK

--- app/server.js
+++ app/server.js
@@ -71,7 +71,7 @@ app.use(function(req, res, next) {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
-    res.locals.errors = req.flash('errors');
+    // res.locals.errors = req.flash('errors');
     res.locals.user = req.user;
     res.locals.data = req.session.data;
     next();
